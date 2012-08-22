da5d239170d5a005a00c86e6d27d65687674ece9
adorsk <adorsk>
2012-08-22 16:39:34 -0400

    added start of file table select for fields, still need to clean this up a bunch

--- sasi_runner/sasi_runner/app/sasi_model_config/views.py
+++ sasi_runner/sasi_runner/app/sasi_model_config/views.py
@@ -1,7 +1,7 @@
 from sasi_runner.app import app
 from sasi_runner.app.sasi_model_config.models import SASIModelConfig
 
-from flask import Blueprint, request, jsonify, render_template, Markup
+from flask import Blueprint, request, jsonify, render_template, Markup, url_for
 
 
 bp = Blueprint('sasi_model_config', __name__, url_prefix='/config',
@@ -26,32 +26,95 @@ def edit(id):
     # Render fields.
     rendered_fields = []
     for field in fields:
-        rendered_field = get_rendered_field(field)
+        rendered_field = render_field(field)
         rendered_fields.append(rendered_field)
 
-    # Clobber assets.
-    assets = {'css': {}, 'js': {}}
+    # Initialize assets with common assets.
+    assets = get_common_assets()
+
+    # Clobber assets from fields.
+    # @TODO: set this from asset categories above.
+    clobbered_assets = {}
     for rendered_field in rendered_fields:
-        field_assets = rendered_field.get('assets', {})
-        for category, category_assets in assets.items():
-            for asset in field_assets.get(category, []):
-                category_assets[asset] = asset
-    for category, category_assets in assets.items():
-        assets[category] = assets[category].values()
+        for asset in rendered_field.get('assets', []):
+            clobbered_assets[asset] = asset
+
+    # Add clobbered assets to global assets.
+    assets.extend(clobbered_assets.keys())
 
     # Render template.
     return render_template("edit.html", config=config, assets=assets, 
                            fields=rendered_fields)
 
-def get_rendered_field(field):
+def render_field(field):
     content = """
-    <h3>%s</h3>
-    """ % field.get('label')
+    <div class="field-section" id="%s">
+    <h3 class="header">%s</h3>
+    <div class="body">
+    <div class="description">The description</div>
+    <div class="widget">The Widget</div>
+    </div>
+    </div>
+    """ % (field.get('id'), field.get('label'))
+
+    script = """
+        require(["jquery","use!backbone","use!underscore", "SASIRunner"],
+        function($, Backbone, _, SASIRunner){
+            var sasi_files = new Backbone.Collection();
+            sasi_files.url = '/sasi_runner/sasi_file/category/testcat/';
+
+            var sasi_file_select_model = new Backbone.Model({
+                choices: sasi_files
+            });
+
+            var sasi_file_select_view = new SASIRunner.views.SASIFileTableSelectView({
+                model: sasi_file_select_model,
+                el: $('.field-section[id="%s"] .widget')
+            });
+
+            $(document).ready(function(){
+                sasi_file_select_view.trigger('ready');
+            });
+        });
+    """ % (field.get('id'))
     return {
         'content': Markup(content),
-        'assets': {
-            'css': ['foo'],
-            'js': ['bar']
-        },
-        'script': ''
+        'assets': [],
+        'script': script
     }
+
+def get_common_assets():
+    return [
+        # require.js
+        format_script_asset(src=url_for('static', filename='js/require_config.js')),
+
+        format_script_asset(src=url_for('static',
+                                    filename='sasi_assets/js/require.js/require.js')),
+
+        # SASIRunner.less
+        format_link_asset(rel='stylesheet/less', href=url_for('static',
+            filename='js/SASIRunner/src/styles/SASIRunner.less')),
+
+        # less.js
+        format_script_asset(src=url_for('static', filename='sasi_assets/js/less.js')),
+
+        # jquery-ui css
+        format_link_asset(rel='stylesheet/less', href=url_for('static',
+            filename='sasi_assets/js/jquery.ui/styles/jquery-ui-1.8.18.custom.css'))
+    ]
+
+def format_script_asset(type_='text/javascript', **kwargs):
+    asset = '<script '
+    for key, value in kwargs.items() + [('type', type_)]:
+        asset += '%s="%s" ' % (key, value)
+    asset += '></script>'
+    return Markup(asset)
+
+def format_link_asset(rel='stylesheet', type_='text/css', **kwargs):
+    asset = '<link '
+    for key, value in kwargs.items() + [('rel', rel), ('type', type_)]:
+        asset += '%s="%s" ' % (key, value)
+    asset += '>'
+    return Markup(asset)
+
+
