26d32740a076444d2ab9ee70b5245f43ffc985bd
adorsk <adorsk>
2012-08-29 10:42:39 -0400

    start of tasks

---
+++ sasi_runner/tasks/__init__.py
@@ -0,0 +1,31 @@
+from threading import Thread
+import time
+
+
+registry = {}
+
+def get_task(task_id):
+    global registry
+    return registry.get(task_id)
+
+def delete_task(task_id):
+    global registry
+    if registry.has_key(task_id):
+        del registry[task_id]
+
+class Task(Thread):
+    def __init__(self):
+        global registry
+        Thread.__init__(self)
+        self.id = id(self)
+        self.status = {
+            'code': None,
+            'data': None,
+            'modified': None
+        }
+        registry[self.id] = self
+
+    def update_status(self, code=None, data=None):
+        self.status['code'] = code
+        self.status['data'] = data
+        self.status['modified'] = time.time()
