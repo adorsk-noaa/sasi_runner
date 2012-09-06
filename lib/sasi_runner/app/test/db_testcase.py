adfd18a8f5fc15bdc282e52f7ba7f9eb853252f6
adorsk <adorsk>
2012-09-06 16:36:51 -0400

    changed db test case to use spatialite

--- lib/sasi_runner/app/test/db_testcase.py
+++ lib/sasi_runner/app/test/db_testcase.py
@@ -1,14 +1,20 @@
+import pyspatialite
+import sys
+sys.modules['pysqlite2'] = pyspatialite
 import unittest
 from sasi_runner.app import app
 from sasi_runner.app import db
+from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 class DBTestCase(unittest.TestCase):
 
     def setUp(self):
-        connection = db.engine.connect()
-        self.trans = connection.begin()
-        db.session = scoped_session(sessionmaker(bind=connection))
+        self.engine = create_engine('sqlite://')
+        self.connection = self.engine.connect()
+        self.connection.execute("SELECT InitSpatialMetaData()") 
+        self.trans = self.connection.begin()
+        db.session = scoped_session(sessionmaker(bind=self.connection))
 
         app.config['TESTING'] = True
         self.client = app.test_client()
