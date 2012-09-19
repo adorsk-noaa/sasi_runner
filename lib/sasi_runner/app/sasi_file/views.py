6e637ce2067e49877779d5200141ad6a559cff15
adorsk <adorsk>
2012-09-19 11:36:09 -0400

    fleshed out files table

--- lib/sasi_runner/app/sasi_file/views.py
+++ lib/sasi_runner/app/sasi_file/views.py
@@ -11,7 +11,7 @@ from datetime import datetime
 
 
 bp = Blueprint('sasi_file', __name__, url_prefix='/sasi_file',
-               template_folder='templates') 
+               template_folder='templates', static_folder='static') 
 
 uploads_dest = os.path.join(sr_config['STORAGE_ROOT'], 'sasi_file')
 uploads = UploadSet('sasifile', extensions=ARCHIVES + TEXT + IMAGES,
@@ -19,7 +19,7 @@ uploads = UploadSet('sasifile', extensions=ARCHIVES + TEXT + IMAGES,
 
 upload_sets = [uploads]
 
-@bp.route('/', methods=['GET'])
+@bp.route('/file/', methods=['GET'])
 @bp.route('/category/<category_id>/', methods=['GET'])
 def list_files(category_id=None):
     q = db.session.query(SASIFile)
@@ -41,21 +41,14 @@ def list_files(category_id=None):
     # Page.
     else:
         assets = get_common_assets()
-        actions = [
-            'delete'
-        ]
-        for i in range(len(actions)):
-            action = actions[i]
-            actions[i] = {'id': action, 'label': action}
 
         return render_template(
             "files_list.html", 
             file_dicts=file_dicts,
             assets=assets, 
-            actions=actions
         ) 
 
-@bp.route('/<int:file_id>', methods=['DELETE'])
+@bp.route('/file/<int:file_id>', methods=['DELETE'])
 def delete_file(file_id):
     f = initialize_file(file_id)
     if f:
@@ -65,17 +58,18 @@ def delete_file(file_id):
     #@TODO: 404 this.
     return "File does not exist."
 
-@bp.route('/<int:file_id>/download', methods=['GET'])
+@bp.route('/file/<int:file_id>/download', methods=['GET'])
 def download_file(file_id):
     f = initialize_file(file_id)
     if f:
-        return redirect(get_file_download_url(f))
+        return redirect(get_file_url(f))
     else:
         #@TODO: 404 this.
         return "File does not exist."
 
+@bp.route('/file/', methods=['POST'])
 @bp.route('/category/<category_id>/file/', methods=['POST'])
-def uploadCategoryFile(category_id):
+def uploadFile(category_id=None):
     if 'sasi_file' in request.files:
         filename = uploads.save(request.files['sasi_file'])
         # Get file info.
@@ -91,23 +85,13 @@ def uploadCategoryFile(category_id):
         file_dict = sasi_file_to_dict(sasi_file)
         return jsonify(file_dict)
 
-@bp.route('/category/<category_id>/', methods=['GET'])
-def getCategoryFiles(category_id):
-    category_files = db.session.query(SASIFile).filter(SASIFile.category ==
-                                                       category_id)
-    file_dicts = []
-    for category_file in category_files:
-        file_dicts.append(sasi_file_to_dict(category_file))
-
-    return Response(json.dumps(file_dicts, indent=2), mimetype='application/json')
-
-def get_file_download_url(sasi_file):
+def get_file_url(sasi_file):
     saved_filename = os.path.basename(sasi_file.path)
     return uploads.url(saved_filename)
 
 def sasi_file_to_dict(sasi_file):
     file_dict = sasi_file.to_dict()
-    file_dict['url'] = get_file_download_url(sasi_file)
+    file_dict['url'] = get_file_url(sasi_file)
     return file_dict
 
 @bp.route('/file/<file_id>/', methods=['GET'])
@@ -119,6 +103,14 @@ def getFileData(file_id):
 def get_common_assets():
     return {
         'styles': [
+            # SASIRunner.less
+            app_util.format_link_asset(rel='stylesheet/less', href=url_for('static',
+                filename='js/SASIRunner/src/styles/SASIRunner.less')),
+
+            # SASIFile.less
+            app_util.format_link_asset(rel='stylesheet/less',
+                href=url_for('.static', filename='styles/SASIFile.less')), 
+
             # jquery-ui css
             app_util.format_link_asset(rel='stylesheet', href=url_for('static', 
                 filename=('sasi_assets/js/jquery.ui/styles/'
